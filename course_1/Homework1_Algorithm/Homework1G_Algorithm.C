d17540a8f0918a54f674099221d6be1ece8785e5
Sergey <Sergeev_sergey99@mail.ru>
2019-07-08 21:22:53 +0300

    it might work

--- course_1/Homework1_Algorithm/Homework1G_Algorithm.C
+++ course_1/Homework1_Algorithm/Homework1G_Algorithm.C
@@ -3,31 +3,59 @@
 //#include <iostream>
 #define LEN 30000000
 
-long x[LEN];
+long A[LEN];
+long B[LEN];
+long q[LEN];
+long i = 0;
+long j = 0;
 
-long min(long a, long b) {
-	if (a < b) {
-		return a;
-	}
-	else {
-		return b;
+void q_push(long n) {
+    A[j++] = n;
+}
+
+long q_pop() {
+    return A[i++];
+}
+
+void q_popLast() {
+    j--;
+}
+
+long q_peekLast() {
+    return A[j - 1];
+}
+
+long q_peekFirst() {
+    return A[i];
+}
+void q_enqueue(long t, long ii) {
+    while (j - i > 0 && q_peekLast() > t) {
+        q_popLast();
+    }
+    B[j] = ii;
+    q_push(t);
+}
+
+void q_dequeue(long ii) {
+	while (j - i > 0 && ii >= B[i]) {
+		q_pop();
 	}
 }
 
 long f(long y) {
-	//(y-z)%2^32 == 0 
-	return ;
+    return y;
 }
 
 
 int main() {
 	FILE* fin; 
 	fopen(&fin,"queue-min.in", "r");
-	//FILE* fout;
-	//fopen(&fout, "stack-min.out", "w");
+	FILE* fout;
+	fopen(&fout, "stack-min.out", "w");
 
 	long n, m, k;
 	long a, b, c;
+	long min = 0;
 
 	fscanf(fin, "%ld", &n);
 	fscanf(fin, "%ld", &m);
@@ -37,22 +65,29 @@ int main() {
 	fscanf(fin, "%ld", &b);
 	fscanf(fin, "%ld", &c);
 
-	long i = 0;
-	for (; i < k; i++) {
+	for (ii = 0; ii < k; ii++) {
 
-		fscanf(fin, "%ld", &x[i]);
+		fscanf(fin, "%ld", &q[ii]);
+	        q_enqueue(q[ii], ii);
 
+	}	
+	for (long ii = k; ii < m - 1; ii++) {
+
+		q[ii] = f(a * q[ii - 2] + b * q[ii - 1] + c);
+		q_enqueue(q[ii], ii);
+		min += q_peekLast();
 	}
-	for (; i < k; i++) {
-
-		x[i] = f(a * x[i - 2] + b * x[i - 1] + c);
-
+	for (long ii = m - 1; ii < n; ii++) {
+		q[ii] = f(a * q[ii - 2] + b * q[ii - 1] + c);
+		q_enqueue(q[ii], ii);
+		min += q_peekFirst();
+		q_dequeue( ii -  m + 1 );
+ 
 	}
 
+	fprintf("%ld", min);
 
 	fclose(fin);
-//	fclose(fout);
-	//fin.close();
-	//fout.close();
+	fclose(fout);
 	return 0;
 }
